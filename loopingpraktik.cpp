cf4166040a147fe8ea36ac1365a824f106d80ab7
chakerox <chakerox@gmail.com>
2026-04-01 12:40:55 +0700

    menggunakan while untuk mengecek angka itu fibonanci atau tidak

--- loopingpraktik.cpp
+++ loopingpraktik.cpp
@@ -24,3 +24,13 @@ bool cekPrima(int x) {
 bool cekFibonacci(int x) {
     int a = 0, b = 1, c = 0;
 
+    while (c <= x) {
+        if (c == x) {
+            return true;
+        }
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return false;
+}
